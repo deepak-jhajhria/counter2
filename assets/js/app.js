48a9f012532f17d4f6fed7ccae658ed650b01dcc
deepakjaat07 <deepakjhajhari4657@gmail.com>
2023-11-15 11:46:37 +0530

    firstcommit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+let addBtn = document.getElementById("addBtn");
+let showAdd = document.getElementById("showAdd");
+let saveBtn = document.getElementById("saveBtn");
+let saveValues = document.getElementById("saveValues");
+let reset = document.getElementById("reset");
+addBtn.addEventListener("click", function () {
+  showAdd.innerHTML = +showAdd.innerHTML + 1;
+});
+saveBtn.addEventListener("click", function () {
+    saveValues.innerHTML +=","+showAdd.innerHTML;
+    showAdd.innerHTML=0;
+});
+
+reset.addEventListener("click", function () {
+  showAdd.innerHTML = 0;
+  saveValues.innerHTML = "";
+});
