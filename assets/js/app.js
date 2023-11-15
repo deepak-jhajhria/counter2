24a014919a8635ad0a20c1a306f6b732abf82e71
deepakjaat07 <deepakjhajhari4657@gmail.com>
2023-11-15 12:37:12 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,17 @@ addBtn.addEventListener("click", function () {
   showAdd.innerHTML = +showAdd.innerHTML + 1;
 });
 saveBtn.addEventListener("click", function () {
-    saveValues.innerHTML += ' '+showAdd.innerHTML;
+  // saveValues.innerHTML += ' '+showAdd.innerHTML;
+  // showAdd.innerHTML=0;
+  if (showAdd.innerHTML == 0) {
+    // alert("enter the value");
+    reset.classList.add("d-none")
+  }
+  else{
+    saveValues.innerHTML+=" "+showAdd.innerHTML;
     showAdd.innerHTML=0;
+    reset.classList.remove("d-none")
+  }
 });
 
 reset.addEventListener("click", function () {
